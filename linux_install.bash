0d742868107398a948c7217aca18b14e30b27724
dm81d1s <dm@essendi.de>
2015-09-21 16:18:02 +0200

    added things to gitignore

--- linux_install.bash
+++ linux_install.bash
@@ -1,2 +1,9 @@
 sudo apt-get update
+
+##install dependencies
 sudo apt-get install texlive texlive-lang-german texlive-doc-de texlive-latex-extra  -y
+
+##install eclipse
+sudo apt-get install eclipse -y
+
+eclipse
