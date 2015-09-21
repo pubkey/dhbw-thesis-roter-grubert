abb4b9fa17aca3abf97d9300129fd1c755546f69
dm81d1s <dm@essendi.de>
2015-09-21 16:58:19 +0200

    lib für quellenangaben

--- linux_install.bash
+++ linux_install.bash
@@ -3,6 +3,9 @@ sudo apt-get update
 ##install dependencies
 sudo apt-get install texlive texlive-lang-german texlive-doc-de texlive-latex-extra  -y
 
+##lib für quellenangaben
+sudo apt-get install texlive-bibtex-extra -y
+
 ##install eclipse
 sudo apt-get install eclipse -y
 
